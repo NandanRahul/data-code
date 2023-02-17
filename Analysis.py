45b4c38cfb975c6dd4b59d32a073fd2c2f1bd2b6
Rahul Nandakumar <108867112+NandanRahul@users.noreply.github.com>
2023-02-17 18:38:51 +0000

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -1,52 +1,68 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+
 data = pd.read_csv('csv1.csv')
-data.sort_values('StartDate')
+
 data['StartDate'] = pd.to_datetime(data.StartDate)
 data['StatusCreatedDate'] = pd.to_datetime(data.StatusCreatedDate)
 data['year'] = pd.DatetimeIndex(data.StartDate).year
+data['month'] = pd.DatetimeIndex(data.StartDate).month
+
+# computing no of days and time left to start date of each event
+data['time_to_event'] = pd.DatetimeIndex(data['StartDate']) - pd.DatetimeIndex(data['StatusCreatedDate'])
+data['days_to_event'] = data['time_to_event'] / np.timedelta64(1, 'D')
+
+# dropping rows where ticket was booked after the start date
+data = data.drop(data[data['StatusCreatedDate'] >= data['StartDate']].index)
+
+# dropping rows where the customer has booked and is not attending the Event
+data_new = data.drop(data[data['BookingStatus'] != 'Attending'].index)
 
 
 def search_by_event_name(event_name_choice):
-    values = data['EventName'].unique()
+    values = data_new['EventName'].unique()
     for i in values:
-        start_date = None
-        end_date = None
         if i == event_name_choice:
-            date = data[data.EventName == i][['StartDate']]
-            dates = data[data.EventName == i][['StatusCreatedDate']]
-            dates.sort_values('StatusCreatedDate')
-            for k in dates:
-                start_date = k
-            for j in date:
-                end_date = j
-            print(start_date, end_date)
-            data_event_based = data[data.EventName == i][['GroupSize']]
-            grouped = data_event_based.groupby(data['StatusCreatedDate'].dt.month)
+            # start date of booking and start date of the event is evaluated below
+            limiting_date_min = data_new.loc[data_new['EventName'].isin([i]), 'StatusCreatedDate'].min()
+            limiting_date_max = data_new.loc[data_new['EventName'].isin([i]), 'StartDate'].max()
+            print(limiting_date_max)
+            print(limiting_date_min)
+            # grouping and summing no of booking according to each month
+            data_event_based = data_new[data_new.EventName == i][['GroupSize']]
+            grouped = data_event_based.groupby(data_new['StatusCreatedDate'].dt.month)
             results = grouped.sum()
             print(results)
+            # trying to plot a bar graph if possible
+            # since if data is null then index error will show up
             try:
                 results.plot(kind='bar')
                 plt.xlabel('Month')
                 plt.ylabel('Bookings')
+                # need to fix position later #todo
+                plt.text(-2, -.7, 'Start date of event: ' + str(limiting_date_max) + '\n'
+                         + 'Booking commencement date: ' + str(limiting_date_max))
                 plt.show()
             except IndexError:
                 results.plot(kind='line')
                 plt.xlabel('Month')
                 plt.ylabel('Bookings')
+                plt.text(-2, -.7, 'Start date of event: ' + str(limiting_date_max) + '\n'
+                         + 'Booking commencement date: ' + str(limiting_date_max))
                 plt.show()
 
 
 def search_by_event_type(event_type_choice):
-    values_event_type = data['EventType'].unique()
+    values_event_type = data_new['EventType'].unique()
     for i in values_event_type:
         if i == event_type_choice:
-            dataframe2021 = data[data.year == 2021][['EventType', 'month', 'GroupSize']]
-            data_event_based = dataframe2021[dataframe2021.EventType == i][['month', 'GroupSize']]
-            grouped1 = data_event_based.groupby('month')
-            dataframe2022 = data[data.year == 2022][['EventType', 'month', 'GroupSize']]
-            data_event_based2 = dataframe2022[dataframe2022.EventType == i][['month', 'GroupSize']]
-            grouped2 = data_event_based2.groupby('month')
+            dataframe2021 = data_new[data_new.year == 2021][['EventType', 'month', 'GroupSize']]
+            data_event_based = dataframe2021[dataframe2021.EventType == i][['GroupSize']]
+            grouped1 = data_event_based.groupby(data['month'])
+            dataframe2022 = data_new[data_new.year == 2022][['EventType', 'month', 'GroupSize']]
+            data_event_based2 = dataframe2022[dataframe2022.EventType == i][['GroupSize']]
+            grouped2 = data_event_based2.groupby(data['month'])
             results1 = grouped1.sum()
             results2 = grouped2.sum()
             print(i)
@@ -66,14 +82,14 @@ def search_by_event_type(event_type_choice):
                 plt.show()
 
 
-data['StartDate'] = pd.to_datetime(data.StartDate)
-data['StatusCreatedDate'] = pd.to_datetime(data.StatusCreatedDate)
-data['date'] = data['StatusCreatedDate'].dt.date
-data['month'] = pd.DatetimeIndex(data.StartDate).month
+# choice 1 means event Name based analysis is given
 choice = int(input('please enter your search method: '))
 if choice == 1:
     eventNameChoice = input("Please enter the preferred Event Name")
     search_by_event_name(eventNameChoice)
+
+# choice 2 gives event type based analysis showing performance throughout the year.
 elif choice == 2:
     eventTypeChoice = input("Please enter your preferred event type")
     search_by_event_type(eventTypeChoice)
+
